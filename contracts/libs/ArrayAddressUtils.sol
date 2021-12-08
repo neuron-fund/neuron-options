318cfd2c8a40d71408b7f1481c2318734f6cb28b
Avele <kotoraxia@gmail.com>
2021-12-08 20:48:17 +0400

    array library function made external to save gas

--- contracts/libs/ArrayAddressUtils.sol
+++ contracts/libs/ArrayAddressUtils.sol
@@ -9,12 +9,11 @@ pragma solidity 0.8.9;
 library ArrayAddressUtils {
     /**
      * @dev uses hashes of array to compare, therefore arrays with different order of same elements wont be equal
-     * TODO make it work for arrays with different order
      * @param self address[]
      * @param arr address[]
      * @return bool
      */
-    function isEqual(address[] memory self, address[] memory arr) internal pure returns (bool) {
+    function isEqual(address[] memory self, address[] memory arr) external pure returns (bool) {
         return keccak256(abi.encodePacked(self)) == keccak256(abi.encodePacked(arr));
     }
 }
