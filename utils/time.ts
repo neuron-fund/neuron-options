42e819428fbee89d731283e83f79bdfa95235f90
Avele <kotoraxia@gmail.com>
2021-12-17 22:40:08 +0400

    utils types and functions

--- utils/time.ts
+++ utils/time.ts
@@ -1,3 +1,5 @@
+import { EthereumProvider } from 'hardhat/types'
+
 export enum Seconds {
   MINUTE = 60,
   HOUR = 3600,
@@ -16,3 +18,10 @@ export const createValidExpiry = days => {
   date.setUTCHours(8, 0, 0, 0)
   return date.getTime() / 1000
 }
+
+export const waitNDays = async (n: number, provider: EthereumProvider) => {
+  await provider.send('evm_increaseTime', [Seconds.DAY * n])
+  await provider.send('evm_mine')
+}
+
+export const waitWeek = (provider: EthereumProvider) => waitNDays(7, provider)
