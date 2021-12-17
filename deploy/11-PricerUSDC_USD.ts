42ec285bbd43ae29f79f1afc488077a28b8f901b
Avele <kotoraxia@gmail.com>
2021-12-17 22:37:41 +0400

    disable pricer set for tests

--- deploy/11-PricerUSDC_USD.ts
+++ deploy/11-PricerUSDC_USD.ts
@@ -26,7 +26,8 @@ const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   // TODO figure out best period values for oracle
   const lockingPeriod = 60 * 10
   const disputePeriod = 60 * 20
-  await oracle.setAssetPricer(asset, pricerDeployResult.address)
+  // TODO reenable setAssetPricerForProduction
+  // await oracle.setAssetPricer(asset, pricerDeployResult.address)
   await oracle.setLockingPeriod(pricerDeployResult.address, lockingPeriod)
   await oracle.setDisputePeriod(pricerDeployResult.address, disputePeriod)
 }
