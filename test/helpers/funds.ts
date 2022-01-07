3bb275dcffa981331b381b83f8312c120b6ae19e
Avele <kotoraxia@gmail.com>
2022-01-07 13:59:11 +0400

    Added more whales for tests

--- test/helpers/funds.ts
+++ test/helpers/funds.ts
@@ -1,6 +1,6 @@
 import { ethers, network } from 'hardhat'
 import { BigNumber } from '@ethersproject/bignumber'
-import { DAI, USDC, USDT } from '../../constants/externalAddresses'
+import { CRV_CVX_ETH, DAI, LIDO_ST_ETH, USDC, USDT } from '../../constants/externalAddresses'
 import { IERC20__factory } from '../../typechain-types'
 import { formatEther } from '@ethersproject/units'
 
@@ -8,6 +8,8 @@ export const whales = {
   [USDC]: '0xe78388b4ce79068e89bf8aa7f218ef6b9ab0e9d0',
   [DAI]: '0x5a16552f59ea34e44ec81e58b3817833e9fd5436',
   [USDT]: '0x5754284f345afc66a98fbb0a0afe71e0f007b949',
+  [CRV_CVX_ETH]: '0x38ee5f5a39c01cb43473992c12936ba1219711ab',
+  [LIDO_ST_ETH]: '0x06920c9fc643de77b99cb7670a944ad31eaaa260',
 } as const
 
 export type AseetsWithWhales = keyof typeof whales
